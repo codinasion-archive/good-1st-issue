e6a8583f2cdb82074e5283989934059261e380df
Harsh Raj <harshraj8843@gmail.com>
2022-11-28 19:09:16 +0530

    try: patch from github workflow

--- backend/script/patch.py
+++ backend/script/patch.py
@@ -1,20 +1,20 @@
 import requests
 import threading
 
-# Access environment variables
-import environ
+import sys
 
-env = environ.Env()
-environ.Env.read_env()
+# get arguments
+BACKEND_URL = sys.argv[1]
+BBACKEND_ACCESS_TOKEN = sys.argv[2]
 
 
 def PATCH():
     # after every 5 minute
     threading.Timer(300.0, PATCH).start()
     res = requests.patch(
-        url=env("BACKEND_URL") + "/good1stissue/",
+        url=BACKEND_URL + "/good1stissue/",
         headers={
-            "Authorization": "Token " + env("BACKEND_ACCESS_TOKEN"),
+            "Authorization": "Token " + BACKEND_ACCESS_TOKEN,
             "Content-Type": "application/json",
         },
     )
