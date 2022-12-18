a6a0a8cacc6ef227bb8c8806278b8e69783e9991
Harsh Raj <harshraj8843@gmail.com>
2022-12-18 20:54:53 +0530

    update logo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 const siteMetadata = {
   title: "Good 1st Issue",
-  description: "A collection of latest 'good first issue' labeled issues for you to start your journey to the amazing world of open-source ❤️",
+  description: "A collection of latest 'good first issue' labeled issues for you to start your journey to the amazing world of open-source ❤️ #GoodFirstIssue #Good1stIssue",
   siteUrl: "https://good1stissue.codinasion.org",
 };
 
